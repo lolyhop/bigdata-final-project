d2cf1d0d31259c920efc3a46a60825cb0d27b58a
Nikita <nik.lizard.mobile@gmail.com>
2026-05-08 19:15:34 +0300

    Add seeds to guarantee reproducibility

--- scripts/train_baseline_models.py
+++ scripts/train_baseline_models.py
@@ -24,6 +24,8 @@ ML_BASELINE_NB_METRICS_PATH = os.environ.get("ML_BASELINE_NB_METRICS_PATH")
 
 ML_BASELINE_EVALUATION_PATH = os.environ.get("ML_BASELINE_EVALUATION_PATH")
 
+SEED = int(os.environ.get("SEED", "42"))
+
 
 def build_spark():
     """Create and return a Spark session connected to YARN.
@@ -53,7 +55,7 @@ def train_random_forest(train_df, test_df):
         featuresCol="features",
         numTrees=10,
         maxDepth=3,
-        seed=42,
+        seed=SEED,
     ).fit(train_df)
 
     predictions = model.transform(test_df)
